5f0701f96364fd3572615654042da99e6ddaacf7
Nicolas Simond <nicolassimond@posteo.ch>
2017-05-28 12:14:27 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "
+#!/usr/bin/env bash
 # Log connection
-sh /opt/ssh-login-alert-telegram/alert.sh" >> /etc/profile
+sh /opt/ssh-login-alert-telegram/alert.sh" > /etc/profile.d/telegram-alert.sh
