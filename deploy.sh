04fa57e7ad4d94f184fd5855a1f09d91aa7ff6d3
Nicolas Simond <nicolassimond@posteo.ch>
2017-05-28 10:43:40 +0200

    Moving repo to /opt/

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "# Log connection" >> /etc/profile
-echo "sh /root/ssh-login-alert-telegram/alert.sh" >> /etc/profile
+echo "sh /opt/ssh-login-alert-telegram/alert.sh" >> /etc/profile
