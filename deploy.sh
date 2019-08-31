659d3252404d461b1ea66f0e8c4403f219e3b889
Valentin Deville <contact@valentin-deville.eu>
2019-08-31 19:06:43 +0200

    Add compatibility with ZSH for deployement

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
+
+add_profiled(){
 cat <<EOF > /etc/profile.d/telegram-alert.sh
 #!/usr/bin/env bash
-# Log connection
-bash /opt/ssh-login-alert-telegram/alert.sh
+# Log connections
+bash $ALERTSCRIPT_PATH
 EOF
+}
+
+add_zsh () {
+cat <<EOF >> /etc/zsh/zshrc
+
+# Log connections
+bash $ALERTSCRIPT_PATH
+EOF
+}
+
+ALERTSCRIPT_PATH="/opt/ssh-login-alert-telegram/alert.sh"
+
+echo "Deploying alerts..."
+add_profiled
+
+echo "Check if ZSH is installed.."
+
+HAS_ZSH=$(grep -o -m 1 "zsh" /etc/shells)
+if [ ! -z $HAS_ZSH ]; then
+    echo "ZSH is installed, deploy alerts to zshrc"
+    add_zsh
+else
+    echo "No zsh detected"
+fi
+
+echo "Done!"
